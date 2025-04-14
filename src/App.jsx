3b845fd9374fc7eef871c41f6fc1205e489b2245
Quwam Binuyo <qbinuyo7@gmail.com>
2025-04-14 11:47:34 +0100

    setting up the enviroment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <Hero />
+        <Projects />
+      </main>
+    </>
+  );
+};
+export default App;
